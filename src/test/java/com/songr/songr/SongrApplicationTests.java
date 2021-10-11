343c23fe1ad713f0e999a0c4f649a6cd2b3a375d
GHADEER KHASAWNEH <GHADEER_KHASAWNEH@YAHOO.COM>
2021-10-11 07:21:46 +0300

    lab11 final version

--- src/test/java/com/songr/songr/SongrApplicationTests.java
+++ src/test/java/com/songr/songr/SongrApplicationTests.java
@@ -16,16 +16,16 @@ class SongrApplicationTests {
 	@Test
 	void testing(){
    Album album = new Album("Jadal", "La Tloo'Al-Daww", 1000, 1000, "https://i.ytimg.com/vi/4ADGYRJ4kIQ/maxresdefault.jpg");
-   album.setTitle("Title");
-   Assertions.assertEquals("Title", album.getTitle());
-   album.setTitle("Title");
-   Assertions.assertEquals("Happiness", album.getTitle());
-   album.setTitle("Title");
-   Assertions.assertEquals(1000, album.getTitle());
-   album.setTitle("Title");
-   Assertions.assertEquals("1000", album.getTitle());
-   album.setTitle("Title");
-   Assertions.assertEquals("Title", album.getTitle());
+//   album.setTitle("Title");
+   Assertions.assertEquals("Jadal", album.getArtist());
+//   album.setArtist("Artist");
+   Assertions.assertEquals("La Tloo'Al-Daww", album.getTitle());
+//   album.setSongCount(1000);
+   Assertions.assertEquals(1000, album.getLength());
+//   album.setLength(1000);
+   Assertions.assertEquals(1000, album.getSongCount());
+//   album.setImageUrl("Title");
+   Assertions.assertEquals("https://i.ytimg.com/vi/4ADGYRJ4kIQ/maxresdefault.jpg", album.getImageUrl());
 
 	}
 
