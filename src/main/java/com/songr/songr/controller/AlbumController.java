4aca1cbd0eb58287702545ee848b5522d95d6de9
GHADEER KHASAWNEH <GHADEER_KHASAWNEH@YAHOO.COM>
2021-10-15 12:46:08 +0300

    lab12

---
+++ src/main/java/com/songr/songr/controller/AlbumController.java
@@ -0,0 +1,49 @@
+package com.songr.songr.controller;
+
+import com.songr.songr.Model.Album;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
+
+@Controller
+
+public class AlbumController {
+
+    @Autowired
+    private AlbumRepository albumRepository;
+
+///// post in database using this route
+
+    @ResponseBody
+    @PostMapping("/v1/albums")
+    public Album createNewPost(@RequestBody Album album) {
+
+        Album newPost = albumRepository.save(album);
+        return newPost;
+    }
+
+
+///   when hitting this route, you'll view the form and also albums coming from database
+
+    @GetMapping("/albums")
+    public String getAlbums(Model model){
+        model.addAttribute("albums", albumRepository.findAll());
+
+        return "albums";
+
+    }
+
+////when clicking submit you'll save albums in database and will be redirected to => albums,,aka same html page
+
+    @PostMapping("/albums")
+    public RedirectView createAlbum(@ModelAttribute Album album) {
+        albumRepository.save(album);
+        return new RedirectView("albums");
+    }
+//
+
+
+
+}
---
+++ src/main/java/com/songr/songr/controller/MainController.java
@@ -0,0 +1,53 @@
+package com.songr.songr.controller;
+
+
+import com.songr.songr.Model.Album;
+import org.springframework.stereotype.*;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+    @Controller
+    public class MainController {
+
+        @GetMapping("/hello")
+        public String greeting() {
+
+            return "hello";
+        }
+
+
+        @GetMapping("/Capitalize/{word}")
+        public String capitalization(@PathVariable String word, Model model) {
+            word.toUpperCase();
+            model.addAttribute("wordHTML", word.toUpperCase());
+            return "capitalization";
+
+        }
+
+
+//        @GetMapping("/album")
+//        public String getClassInformation(Model model) {
+//            List<Album> list = new ArrayList<>();
+//            Album Album1 = new Album("Jadal", "La Tloo' Al-Daww", 1000, 1000, "https://i.ytimg.com/vi/4ADGYRJ4kIQ/maxresdefault.jpg");
+//            Album Album2 = new Album("Julia Boutross", "Yawman ma", 1000, 1000, "https://i1.sndcdn.com/artworks-000025826032-rfb25v-t500x500.jpg");
+//            Album Album3 = new Album("Rami Esam", "Segni-Bil-Alwan", 1000, 1000, "https://lyricstranslate.com/files/styles/large/public/12523840_1030903076953677_6196382277839151779_n.jpg?itok=vOQZhaJf");
+//
+//
+//            list.add(Album1);
+//            list.add(Album2);
+//            list.add(Album3);
+//            model.addAttribute("list", list);
+//
+//            return "albums";
+//        }
+//
+//    }
+
+    }
